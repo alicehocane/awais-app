e0574a996705d9f6126ef739f4b0da7d91660ca0
Awais Karni <35777843+alicehocane@users.noreply.github.com>
2025-04-05 21:23:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get("*", (req, res) => {
 });
 
 // Use the port from Railway or fallback to 3000
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
